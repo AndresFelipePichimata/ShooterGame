8a54c61c8eea0e0eeb288f0c6f181e60d5cad605
Andrés Felipe Pichimata <andrespira@unisabana.edu.co>
2022-02-27 19:01:27 -0500

    a
    disparo normal y prefabs

---
+++ Assets/Scripts/EnemySpecs.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpecs : MonoBehaviour
+{
+    [SerializeField] float hp;
+    Transform transform;
+    Rigidbody2D body;
+    [SerializeField] float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate() {
+        float newSpeed;
+        body.velocity = new Vector2(speed, body.velocity.y);
+        Debug.Log("x: " + transform.position.x);
+        
+        if (transform.position.x >= 15)
+        {
+            newSpeed = Math.Abs(speed) * (-1);
+            speed = newSpeed;
+        }
+        if (transform.position.x <= -9.5)
+        {
+            speed = Math.Abs(speed);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        GameObject go = other.gameObject;
+        if (go.tag == "Bullet")
+        {
+         hp--;
+        }
+        if (hp == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
